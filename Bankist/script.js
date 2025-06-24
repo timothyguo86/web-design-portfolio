edc133cd2d55059dc3b8b97957c65e39656e874f
timothyguo86 <timothyguo86@gmail.com>
2025-06-23 23:39:47 -0400

    [feat] Add smooth scrolling and update footer credits
    Introduced smooth scrolling animation to the "Learn More" button for better UX.

--- Bankist/script.js
+++ Bankist/script.js
@@ -29,3 +29,13 @@ document.addEventListener('keydown', function (e) {
     closeModal()
   }
 })
+
+///////////////////////////////////////
+// Smooth scrolling animation
+
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' })
+})
