c6059bf86a4fd05227213889a8c1fc2e5d752510
timothyguo86 <timothyguo86@gmail.com>
2025-06-27 23:11:15 -0400

    Implement map and geolocation functionality in script.js

--- Mapty/script.js
+++ Mapty/script.js
@@ -22,3 +22,35 @@ const inputDistance = document.querySelector('.form__input--distance')
 const inputDuration = document.querySelector('.form__input--duration')
 const inputCadence = document.querySelector('.form__input--cadence')
 const inputElevation = document.querySelector('.form__input--elevation')
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords
+      const cords = [latitude, longitude]
+
+      const map = L.map('map').setView(cords, 13)
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+
+      map.on('click', mapEvent => {
+        const { lat, lng } = mapEvent.latlng
+        const popupProperties = {
+          autoClose: false,
+          maxWidth: 250,
+          minWidth: 100,
+          closeOnClick: false,
+          className: 'cycling-popup',
+          title: 'Running'
+        }
+
+        L.marker([lat, lng]).addTo(map).bindPopup(L.popup(popupProperties)).setPopupContent('test').openPopup()
+      })
+    },
+    error => {
+      alert('Could not get your location')
+    }
+  )
+}
