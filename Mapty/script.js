cec62459a0754e70462153da456c1cec257414d4
timothyguo86 <timothyguo86@gmail.com>
2025-06-27 23:44:56 -0400

    Implement form handling and workout submission functionality

--- Mapty/script.js
+++ Mapty/script.js
@@ -23,30 +23,25 @@ const inputDuration = document.querySelector('.form__input--duration')
 const inputCadence = document.querySelector('.form__input--cadence')
 const inputElevation = document.querySelector('.form__input--elevation')
 
+let map
+let mapEvent
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     position => {
       const { latitude, longitude } = position.coords
       const cords = [latitude, longitude]
 
-      const map = L.map('map').setView(cords, 13)
+      map = L.map('map').setView(cords, 13)
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map)
 
-      map.on('click', mapEvent => {
-        const { lat, lng } = mapEvent.latlng
-        const popupProperties = {
-          autoClose: false,
-          maxWidth: 250,
-          minWidth: 100,
-          closeOnClick: false,
-          className: 'cycling-popup',
-          title: 'Running'
-        }
-
-        L.marker([lat, lng]).addTo(map).bindPopup(L.popup(popupProperties)).setPopupContent('test').openPopup()
+      map.on('click', mapE => {
+        mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
       })
     },
     error => {
@@ -54,3 +49,25 @@ if (navigator.geolocation) {
     }
   )
 }
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+
+  const { lat, lng } = mapEvent.latlng
+  const popupProperties = {
+    autoClose: false,
+    maxWidth: 250,
+    minWidth: 100,
+    closeOnClick: false,
+    className: 'cycling-popup',
+    title: 'Running'
+  }
+
+  L.marker([lat, lng]).addTo(map).bindPopup(L.popup(popupProperties)).setPopupContent('test').openPopup()
+})
+
+inputType.addEventListener('change', () => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
